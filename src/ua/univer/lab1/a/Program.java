be4e542c84dd0c7560e7b48f8180d4d16304af71
mmveres <mmveres@gmail.com>
2021-09-02 15:48:56 +0300

    lesson01.1

--- src/ua/univer/lab1/a/Program.java
+++ src/ua/univer/lab1/a/Program.java
@@ -9,49 +9,52 @@ import java.awt.event.MouseListener;
 
 public class Program {
     static Thread th;
+
     public static void main(String[] args) {
         JFrame win = new JFrame();
         win.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         win.setSize(500, 400);
+
+        JPanel panel = getjPanel();
+
+        win.setContentPane(panel);
+        win.setVisible(true);
+    }
+
+    private static JPanel getjPanel() {
         JPanel panel = new JPanel();
         JTextField text = new JTextField("                    ");
         JButton btn = new JButton("Ok");
         JSlider slider = new JSlider();
-        btn.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-                th = new Thread(new Runnable() {
-                    @Override
-                    public void run() {
+        btn.addActionListener(e -> {
+            th = new Thread(
+                    () -> {
                         for (int i = 0; i < 100; i++) {
-                            text.setText(th.getPriority()+" : " + i);
-                            slider.setValue(100-i);
+                            text.setText(th.getPriority() + " : " + i);
+                            slider.setValue(100 - i);
                             try {
                                 Thread.sleep(100);
                             } catch (InterruptedException interruptedException) {
                                 interruptedException.printStackTrace();
                             }
                         }
-                    }
-                });
-                th.setPriority(Thread.NORM_PRIORITY);
-                th.start();
-            }
+                    });
+            th.setPriority(Thread.NORM_PRIORITY);
+            th.start();
         });
 
         JButton bPlus = new JButton("+");
         bPlus.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                th.setPriority(th.getPriority()+1);
+                th.setPriority(th.getPriority() + 1);
             }
         });
         JButton bMinus = new JButton("-");
         bMinus.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                th.setPriority(th.getPriority()-1);
+                th.setPriority(th.getPriority() - 1);
             }
         });
         panel.add(bMinus);
@@ -59,8 +62,6 @@ public class Program {
         panel.add(btn);
         panel.add(text);
         panel.add(slider);
-
-        win.setContentPane(panel);
-        win.setVisible(true);
+        return panel;
     }
 }
